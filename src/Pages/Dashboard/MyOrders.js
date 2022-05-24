63be54120dac05a485e34579b30a81a9e8127506
MagicManik <manikislam1952@gmail.com>
2022-05-25 01:18:03 +0600

    my order done

--- src/Pages/Dashboard/MyOrders.js
+++ src/Pages/Dashboard/MyOrders.js
@@ -1,9 +1,77 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import { Link, useNavigate } from 'react-router-dom';
+import { signOut } from 'firebase/auth';
 
 const MyOrders = () => {
+
+    const [orders, setOrders] = useState([]);
+    const [user] = useAuthState(auth);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (user) {
+            fetch(`http://localhost:5000/order?user=${user.email}`, {
+                method: 'GET',
+                headers: {
+                    'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+                }
+            })
+                .then(res => {
+                    console.log('res', res);
+                    if (res.status === 401 || res.status === 403) {
+                        signOut(auth);
+                        localStorage.removeItem('accessToken');
+                        navigate('/');
+                    }
+                    return res.json()
+                })
+                .then(data => {
+
+                    setOrders(data);
+                });
+        }
+    }, [user])
+
     return (
         <div>
-            <h1>This is my orders outlet</h1>
+            <h2>My Orders: {orders.length}</h2>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Product Name</th>
+                            <th>Cancle</th>
+                            <th>Pay Now</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            orders.map((order, index) =>
+                                <tr user={user} key={order._id}>
+                                    <th>{index + 1}</th>
+                                    <td>{console.log(user)}</td>
+                                    <td>{order.date}</td>
+                                    <td>{order.slot}</td>
+                                    <td>{order.treatment}</td>
+                                    <td>
+                                        {(order.price && !order.paid) && <Link to={`/dashboard/payment/${order._id}`}><button className='btn btn-xs btn-success'>pay</button></Link>}
+                                        {(order.price && order.paid) && <div>
+                                            <p><span className='text-success'>Paid</span></p>
+                                            <p>Transaction id: <span className='text-success'>{order.transactionId}</span></p>
+                                        </div>}
+                                    </td>
+                                </tr>)
+                        }
+
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
--- src/Pages/Dashboard/MyReview.js
+++ src/Pages/Dashboard/MyReview.js
@@ -3,7 +3,7 @@ import React from 'react';
 const MyReview = () => {
     return (
         <div>
-            <h1>This my review outlet</h1>
+            <h1>hel</h1>
         </div>
     );
 };
